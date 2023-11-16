fac03f2004c5eb2cc154cf6210cd13bc0b5dc27d
Abdallah 616 <yassinabdou59@gmail.com>
2023-11-16 08:09:07 -0800

    task solved

---
+++ new.c
@@ -0,0 +1,81 @@
+#include "monty.h"
+/**
+ * main - Entry point
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	stack_t *stack = NULL;
+	char *opcode = malloc(MAX_OPCODE_LENGTH);
+    int found, i;
+    unsigned int line_number = 1;
+	FILE *file;
+	instruction_t instructions[] = {
+        {"push", push},
+        {"pall", pall},
+        {"pint", pint},
+        {"pop", pop},
+        {"swap", swap},
+        {"add", add},
+        {"nop", nop},
+        {"sub", sub},
+        {"div", div_op},
+        {"mul", mul_op},
+        {"mod", mod_op},
+        {NULL, NULL}
+    };
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+
+
+
+    while (fscanf(file, "%99s", opcode) != EOF)
+    {
+        if (opcode[0] == '#')
+        {
+            fgets(opcode, MAX_OPCODE_LENGTH, file);
+            line_number++;
+            continue;
+        }
+
+        found = 0;
+        for (i = 0; instructions[i].opcode != NULL; i++)
+        {
+            if (strcmp(opcode, instructions[i].opcode) == 0)
+            {
+                instructions[i].f(&stack, line_number);
+                found = 1;
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+            fclose(file);
+            free(opcode);
+            free(stack);
+            exit(EXIT_FAILURE);
+        }
+
+        line_number++;
+    }
+
+    fclose(file);
+    free(opcode);
+    free(stack);
+
+    return 0;
+}
---
+++ opf3.c
@@ -0,0 +1,53 @@
+#include "monty.h"
+/**
+ * mul_op - multiplies the top two elements of the stack
+ * @stack: pointer to the stack
+ * @line_number: line number in the file
+ */
+void mul_op(stack_t **stack, unsigned int line_number)
+{
+    stack_t *top1, *top2;
+    if (*stack == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    top1 = *stack;
+    top2 = top1->next;
+    top2->n *= top1->n;
+    *stack = top2;
+    (*stack)->prev = NULL;
+    free(top1);
+}
+/**
+ * mod_op - computes the rest of the division of the second top element
+ *           by the top element of the stack
+ * @stack: pointer to the stack
+ * @line_number: line number in the file
+ */
+void mod_op(stack_t **stack, unsigned int line_number)
+{
+    stack_t *top1, *top2;
+
+    if (*stack == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    top1 = *stack;
+    top2 = top1->next;
+
+    if (top1->n == 0)
+    {
+        fprintf(stderr, "L%u: division by zero\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    top2->n %= top1->n;
+
+    *stack = top2;
+    (*stack)->prev = NULL;
+
+    free(top1);
+}
